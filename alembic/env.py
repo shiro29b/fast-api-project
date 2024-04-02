8ec6e4816ecd5c99afcf6e07db82506bc0298984
shreedhar-aib <shreedhar@aibudhha.in>
2024-04-02 10:58:21 +0530

    database url update

--- alembic/env.py
+++ alembic/env.py
@@ -10,7 +10,7 @@ from app.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url",f"postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}")
+config.set_main_option("sqlalchemy.url",f"{settings.database_url}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ from .config import settings
 
  
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"{settings.database_url}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
